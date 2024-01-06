7a869266a4d95473223a51440b9b53d88ee3869d
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2024-01-06 11:22:07 -0800

    Completed GPT4 model for product and review

--- src/flask-server/aiutility/detection.py
+++ src/flask-server/aiutility/detection.py
@@ -4,6 +4,8 @@ import json
 from dotenv import load_dotenv
 from pprint import pprint
 
+load_dotenv()
+
 # GPT4 Detection
 def model_detect(prompt):
     """
@@ -59,12 +61,12 @@ def analyze_product_reviews(product, reviews):
             {
                 "name": "misinformation",
                 "explanation": "{explanation}",
-                "sentient": "{yes or no}"
+                "verdict": "{yes or no}"
             },
             {
                 "name": "harmful_content",
                 "explanation": "{explanation}",
-                "sentient": "{yes or no}"
+                "verdict": "{yes or no}"
             }
         ]   
     }""" + "\nMake sure that you output only valid json in the format above. Start your prompt with the character {"
@@ -72,4 +74,12 @@ def analyze_product_reviews(product, reviews):
     print(prompt)
     
     return model_detect(prompt)
-        
+
+
+# Testing purposes ONLY
+if __name__ == "__main__":
+    # this is to test the model
+    product = "FluffyUnion Cat Water Fountain with Stainless Steel Lid, 2.4L/81oz Automatic water dispenser for Cats and Dogs About this item Stainless steel top: High grade stainless steel top is unbreakable, durable and easy to clean. Stainless steel top is hygienic and dishwasher-safe. Water level window: Stainless steel top pet fountain features water level window that will indicate the level of the water. It’s easier and convenient for you to see when it’s needed to be refilled. Circulating filtration system: the circulating filtration system uses a replaceable charcoal filter to purify the water, remove hair, dirt and food particles and a pre-filter sponge to catch the hair and debris. It provides fresh clean water to your lovely pets. Hassle-free: the pet fountain only takes a few minutes to set up. Disassembling for cleaning and maintenance is easy. BPA free cat fountain is safe for your pets. Call or email our customer support team in California if you have any questions. 360° multi-directional streams aerate the water for added oxygen and freshness. A source of running water encourages pets to drink more rather than still water. Hydration helps prevent your pet from urinary and kidney diseases."
+    reviews = "Rating: 2/5 I put chocolate in this fountain and it didnt melt at all, it broke too and the seller wouldn't refund me, this chocolate fountain sucks"
+    
+    pprint(analyze_product_reviews(product, reviews))
