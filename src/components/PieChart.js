23777a8ca8a2385445da59e7c60fb60f94bf5729
Oscar Zhang <oscarzhang228@gmail.com>
2024-01-06 13:26:18 -0500

    pie chart

---
+++ src/components/PieChart.js
@@ -0,0 +1,25 @@
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+export const PieChart = ({ data }) => {
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
+};
