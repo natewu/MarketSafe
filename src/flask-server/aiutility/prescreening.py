9f76612f98268a4c935a563b513a72ea2e7774be
AnaCoda <anacod@zoho.com>
2024-01-06 12:43:21 -0700

    Hate speech detection

--- src/flask-server/aiutility/prescreening.py
+++ src/flask-server/aiutility/prescreening.py
@@ -1,7 +1,18 @@
 # Perspective API
+"""
+pip install google-api-python-client
+pip install python-dotenv
+pip install google-cloud-bigquery
+Ask Ana for amazonhatespeech-1857bd410110.json and put it in this aiutility folder
+"""
 from googleapiclient import discovery
+from google.cloud.bigquery.client import Client
+import os
 import json
 from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "src/flask-server/aiutility/amazonhatespeech-1857bd410110.json"
 
 load_dotenv()
 
@@ -21,5 +32,131 @@ def analyze_review(review):
     response = client.comments().analyze(body=analyze_request).execute()
     return response
 
-result = analyze_review("This product sucks, it easily broke and I had to return it twice.")
+result = analyze_review("This product is bad, it easily broke and I had to return it twice.")
 print(json.dumps(result, indent=2))
+
+"""
+Output format:
+{
+  "attributeScores": {
+    "PROFANITY": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.06646215,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.06646215,
+        "type": "PROBABILITY"
+      }
+    },
+    "THREAT": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.007521313,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.007521313,
+        "type": "PROBABILITY"
+      }
+    },
+    "INSULT": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.06512285,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.06512285,
+        "type": "PROBABILITY"
+      }
+    },
+    "IDENTITY_ATTACK": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.0061416444,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.0061416444,
+        "type": "PROBABILITY"
+      }
+    },
+    "TOXICITY": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.20056234,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.20056234,
+        "type": "PROBABILITY"
+      }
+    },
+    "SEXUALLY_EXPLICIT": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.012739069,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.012739069,
+        "type": "PROBABILITY"
+      }
+    },
+    "SEVERE_TOXICITY": {
+      "spanScores": [
+        {
+          "begin": 0,
+          "end": 66,
+          "score": {
+            "value": 0.0037574768,
+            "type": "PROBABILITY"
+          }
+        }
+      ],
+      "summaryScore": {
+        "value": 0.0037574768,
+        "type": "PROBABILITY"
+      }
+    }
+  },
+  "languages": [
+    "en"
+  ],
+  "detectedLanguages": [
+    "en"
+  ]
+}
+"""
