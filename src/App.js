069ed4cc1cd176689d44b3098d37961df42570d3
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2024-01-07 01:28:54 -0800

    COOKING

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./global.scss";
 
-import { Link, Route, Routes } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Account from "./routes/Account";
 import { Collections } from "./routes/Collections";
 import { Dashboard } from "./routes/Dashboard";
 import Home from "./routes/Home";
-import Login from "./routes/Login";
 import NavBar from "./Nav";
 import ProductPage from "./routes/ProductPage";
 import Products from "./routes/Products";
 import React from "react";
 import axios from "axios";
 import styles from "./App.module.scss";
+import Login from "./routes/Login"; 
 
 function App() {
   const [products, changeProducts] = useState([]);
-  // current collections
   const [collections, changeCollections] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     // disable text select
@@ -36,32 +36,20 @@ function App() {
     });
   }, []);
 
+  const showNavBar = location.pathname !== '/login';
+
   return (
     <div className={`${styles.App}`}>
-      <NavBar />
+      {showNavBar && <NavBar />} {/* Conditionally render NavBar */}
       <div className={`${styles.content}`}>
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route
-            path="/products"
-            element={
-              <Products products={products} changeProducts={changeProducts} />
-            }
-          ></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/products" element={<Products products={products} changeProducts={changeProducts} />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route
-            path="/collections"
-            element={
-              <Collections
-                changeProducts={changeProducts}
-                changeCollections={changeCollections}
-                collections={collections}
-              />
-            }
-          ></Route>
-          <Route path="/account" element={<Account />}></Route>
+          <Route path="/collections" element={<Collections changeProducts={changeProducts} changeCollections={changeCollections} collections={collections} />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} /> {/* Separate route for Login */}
         </Routes>
       </div>
     </div>
