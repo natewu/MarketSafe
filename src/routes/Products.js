4bcaaa46128c8d727572c188c89cb19e424599a6
natewu <36091727+natewu@users.noreply.github.com>
2024-01-06 17:49:05 -0700

    review count

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -303,7 +303,7 @@ export function Product({ product }) {
       <div className={styles.product__info}>
         <p className={styles.product__title}>{product.title}</p>
         <p className={styles.product__desc}>{product.description}</p>
-        {/* <p classname={styles.product__reviews}>{}</p> */}
+        <p className={styles.product__reviews}>Reviews: {product.reviews.length}</p>
       </div>
     </div>
   );
